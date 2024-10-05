beabb4cb9ef6335906d6ff4a47939b8fa5756bca
S10may1999 <sandeepsharma89244@gmail.com>
2024-10-05 12:19:13 +0530

    cors updated for all devices

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ const app=express();
 
 connectDB();
 
-app.use(cors());
+app.use(cors(
+    {
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    }
+));
 
 app.use(express.json());
 
